d27a4e25f4fd986701361e5094baae1a4bdca128
HammadSamad <154352602+HammadSamad@users.noreply.github.com>
2024-03-12 12:10:07 +0500

    Add files via upload

---
+++ if or else/script.js
@@ -0,0 +1,57 @@
+let age = prompt("Enter your age");
+
+
+if (age == 18) {
+    
+    console.log("you are applicable for driving license");
+}
+
+else if (age < 18){
+    console.log("you are not applicable for driving license");
+}
+
+
+else if (age >= 80 ){
+    console.log("your driving license expired")
+}
+
+
+else if (age >= 18){
+    console.log("you are applicable for driving license")
+}
+
+
+else{
+    console.log("you entered invalid value")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
