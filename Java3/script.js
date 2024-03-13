fbf93a75ae6c3279e31f940bf808c0994b72c940
HammadSamad <154352602+HammadSamad@users.noreply.github.com>
2024-03-13 20:50:59 +0500

    Add files via upload

---
+++ Java3/script.js
@@ -0,0 +1,97 @@
+let grade = prompt("Enter your Marks")
+console.log(grade)
+
+if (grade === 700) {
+    console.log("Grade A1+ 90% Pass")
+}
+
+else if (grade = 600) {
+    console.log("Grade A+ 80% Pass")
+}
+
+
+else if (grade = 500) {
+    console.log("Grade A 70% Pass")
+}
+
+else if (grade = 400) {
+    console.log("Grade B 60% Pass")
+}
+
+else if (grade = 300){
+    console.log("Grade C 50% Pass")
+}
+
+else if (grade = 250){
+    console.log("Grade D 40% Pass")
+}
+
+else if (grade <= 249){
+    console.log("Fail")
+}
+
+else {
+    console.log("Invalid Number")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
