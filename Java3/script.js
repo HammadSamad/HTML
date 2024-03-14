322cb58f6abc38dcafe00e95e488cffc8d9f86d7
HammadSamad <154352602+HammadSamad@users.noreply.github.com>
2024-03-14 20:53:03 +0500

    Add files via upload

---
+++ Java3/script.js
@@ -0,0 +1,59 @@
+let grade = prompt("Enter your Percentage")
+console.log(grade)
+
+if (grade == 90) {
+    console.log("Grade A1+")
+}
+
+else if (grade >= 90){
+    console.log("Grade A1+")
+}
+
+else if (grade >= 80) {
+    console.log("Grade A+")
+}
+
+else if (grade >= 70) {
+    console.log("Grade A")
+}
+
+else if (grade >= 60) {
+    console.log("Grade B")
+}
+
+else if (grade >= 50){
+    console.log("Grade C")
+}
+
+else if (grade >= 40){
+    console.log("Grade D")
+}
+
+else if (grade < 40){
+    console.log("Fail")
+}
+
+else {
+    console.log("Invalid Number")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
