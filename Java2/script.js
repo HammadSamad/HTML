6d7f77f88118e8985d49c562ec56a534928a6c2f
HammadSamad <154352602+HammadSamad@users.noreply.github.com>
2024-03-14 20:52:50 +0500

    Add files via upload

---
+++ Java2/script.js
@@ -0,0 +1,32 @@
+let num1 = prompt("Enter First Number");
+console.log(num1)
+
+let num2 = prompt("Enter Second Number");
+console.log(num2)
+
+let num3 = prompt("Enter Third Number");
+console.log(num3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
