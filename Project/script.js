deffdfcb0f7808a9cef3460764f873c86de5ef54
HammadSamad <154352602+HammadSamad@users.noreply.github.com>
2024-05-06 23:03:52 +0500

    Add files via upload

---
+++ Project/script.js
@@ -0,0 +1,23 @@
+// java script for responsive navbar //
+
+const menuBtn = document.querySelector(".menu-btn")
+const navbigation = document.querySelector(".navbar")
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navbigation.classList.toggle("acitve");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
