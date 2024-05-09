93222e252468170fe64d39f42e2d25444c724f64
HammadSamad <154352602+HammadSamad@users.noreply.github.com>
2024-05-09 22:05:14 +0500

    Add files via upload

--- Project1/script.js
+++ Project1/script.js
@@ -2,12 +2,10 @@
 
 let menuBtn = document.querySelector(".menu-btn");
 let navbigation = document.querySelector(".navbar");
-let transform = document.querySelector(".navbar.active")
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     navbigation.classList.toggle("navbar");
-    transform.classList.toggle("active");
 })
 
 
