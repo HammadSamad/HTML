83d018c7d5740b3a74ba75b182ebb01071d6a054
HammadSamad <mhammadsamad178@gmail.com>
2024-07-10 00:23:00 +0500

    edit files

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ menu.addEventListener("click", () => {
 });
 
 document.addEventListener("click", (e) => {
-    if(!menu.contains(e.target) && e.target !== navbar) {
+    if(!navbar.contains(e.target) && e.target !== menu) {
         menu.classList.remove('active');
         navbar.classList.remove("active");
     };
@@ -55,8 +55,29 @@ document.addEventListener("click", (e) => {
 
 // navbar work end
 
+// dropdown work start
+
+let dropToggle = document.querySelector(".drop-n");
+let dropDown = document.querySelector(".drop");
+let dropSvg = document.querySelector(".drop-svg")
 
 
+dropToggle.addEventListener("click", () => {
+    dropToggle.classList.toggle("active");
+    dropDown.classList.toggle("active");
+    dropSvg.classList.toggle("active");
+});
+
+document.addEventListener("click", (e) => {
+    if(!dropToggle.contains(e.target) && e.target !== dropDown) {
+        dropToggle.classList.remove('active');
+        dropDown.classList.remove("active");
+        dropSvg.classList.remove("active");
+    };
+});
+
+
+// dropdown work end 
 
 
 
